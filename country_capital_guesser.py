7731269970525941299e36470195d35936fab6a8
Nathan <nathan@musicnate.ca>
2016-05-20 15:21:25 -0400

    Ready for clipart additions.

--- country_capital_guesser.py
+++ country_capital_guesser.py
@@ -5,23 +5,38 @@ import easygui
 # This is called a python dictionary. It has keys (Canada, France etc...) and Values (Paris, Ottawa)
 countries_capitals = {"Canada": "Ottawa", "United States": "Washington", "France": "Paris"}
 
-def ask_to_play():
-    easygui.ynbox("Do you want to play a game?", "Country Guesser", ("Yes", "No"))
+def ask_to_play(results):
+    return easygui.ynbox("Do you want to play a game?", "Country Guesser", ("Yes", "No"))
 
-# The variable "playing" determines if we will play the game. 1 = yes, 0 = no
-playing = 1
-correct_answers = 0
-total_questions = 0
-correct_percent = 0
+def main_question_box(country):
+    return easygui.enterbox("What is the capital of: " + country + "?", "Country Capital Guesser!!")
+
+def score_screen(correct_answers, total_questions):
+    correct_percent = round(((correct_answers / total_questions) * 100), 2)
+    if correct_percent >= 80:
+        #ask_to_play()
+        print(":D")
+    elif correct_percent >= 60:
+        print(":)")
+    elif correct_percent >= 40:
+        print(":(")
+    else:
+        print(":'(")
 
 ###############################################
 # Main
 ###############################################
 
-def __main__():
+def funtime():
+    playing = 1
+    correct_answers = 0
+    total_questions = 0
+    correct_percent = 0
+    ask_to_play()
     while playing:
         for key, value in countries_capitals.items():
-            answer = input("Name the capital of: " + key + "\n").lower()
+            answer = main_question_box(key)
+            # answer = input("Name the capital of: " + key + "\n").lower()
             total_questions += 1 # Short for total_questions = total_questions + 1
 
             if answer == countries_capitals[key] or answer.title() == countries_capitals[key]:
@@ -37,7 +52,7 @@ def __main__():
         else:
             playing = 0
 
-    correct_percent = round(((correct_answers / total_questions) * 100), 2)
+    score_screen(correct_answers, total_questions)
     print("You scored " + str(correct_answers)+ "/" + str(total_questions) + " (" + str(correct_percent) + "%)")
 
 ###############################################
@@ -45,15 +60,8 @@ def __main__():
 ###############################################
 
 def test_1():
-    if correct_percent >= 80:
-        print(":D")
-    elif correct_percent >= 60:
-        print(":)")
-    elif correct_percent >= 40:
-        print(":(")
-    else:
-        print(":'(")
-
-
-ask_to_play()
+    pass
+# ask_to_play()
+# main_question_box("Canada")
     
+funtime()
