4cef0f2b54b40f55e08f7c5e1059b7cd4abfd042
Nathan <nathan@musicnate.ca>
2016-05-20 14:32:03 -0400

    Update ToDo and add easygui for testing by KJ

--- country_capital_guesser.py
+++ country_capital_guesser.py
@@ -1,7 +1,12 @@
 #! /usr/bin/env python3
 
+import easygui
+
 # This is called a python dictionary. It has keys (Canada, France etc...) and Values (Paris, Ottawa)
 countries_capitals = {"Canada": "Ottawa", "United States": "Washington", "France": "Paris"}
+
+def ask_to_play():
+    easygui.ynbox("Do you want to play a game?", "Country Guesser", ("Yes", "No"))
 
 # The variable "playing" determines if we will play the game. 1 = yes, 0 = no
 playing = 1
@@ -9,35 +14,47 @@ correct_answers = 0
 total_questions = 0
 correct_percent = 0
 
-while playing:
-    for key, value in countries_capitals.items():
-        answer = input("Name the capital of: " + key + "\n").lower()
-        total_questions += 1 # Short for total_questions = total_questions + 1
-        
-        if answer == countries_capitals[key] or answer.title() == countries_capitals[key]:
-            correct_answers += 1 
-            print("Correct!")
+###############################################
+# Main
+###############################################
+
+def __main__():
+    while playing:
+        for key, value in countries_capitals.items():
+            answer = input("Name the capital of: " + key + "\n").lower()
+            total_questions += 1 # Short for total_questions = total_questions + 1
+
+            if answer == countries_capitals[key] or answer.title() == countries_capitals[key]:
+                correct_answers += 1 
+                print("Correct!")
+            else:
+                print("Wrong!")
+
+        # Should we keep playing?
+        response = input("Would you like to play again?: \n")
+        if response.lower() == "yes" or response == "y":
+            playing = 1
         else:
-            print("Wrong!")
+            playing = 0
 
-    # Should we keep playing?
-    response = input("Would you like to play again?: \n")
-    if response.lower() == "yes" or response == "y":
-        playing = 1
+    correct_percent = round(((correct_answers / total_questions) * 100), 2)
+    print("You scored " + str(correct_answers)+ "/" + str(total_questions) + " (" + str(correct_percent) + "%)")
+
+###############################################
+# Testing
+###############################################
+
+def test_1():
+    if correct_percent >= 80:
+        print(":D")
+    elif correct_percent >= 60:
+        print(":)")
+    elif correct_percent >= 40:
+        print(":(")
     else:
-        playing = 0
-
-correct_percent = round(((correct_answers / total_questions) * 100), 2)
-print("You scored " + str(correct_answers)+ "/" + str(total_questions) + " (" + str(correct_percent) + "%)")
-
-if correct_percent >= 80:
-    print(":D")
-elif correct_percent >= 60:
-    print(":)")
-elif correct_percent >= 40:
-    print(":(")
-else:
-    print(":'(")
+        print(":'(")
 
 
+def test_2():
+    ask_to_play()
     
